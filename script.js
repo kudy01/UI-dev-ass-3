ea5c6384d9e8f5c93fba9faeb955a209f9a80953
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2022-05-23 21:12:17 +1000

    feat: Added cart page and fixed design

---
+++ script.js
@@ -0,0 +1,83 @@
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function toggleDropdown1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+function toggleDropdown2() {
+  document.getElementById("myDropdown2").classList.toggle("newShow");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("newShow")) {
+        openDropdown.classList.remove("newShow");
+      }
+    }
+  }
+};
+
+function toggleDropdown3() {
+  document.getElementById("myDropdown3").classList.toggle("newShow");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("newShow")) {
+        openDropdown.classList.remove("newShow");
+      }
+    }
+  }
+};
+
+function showImage() {
+  document.getElementById("image").style.display = "inline";
+
+  document.getElementById("buttonId").className = "selected-button";
+}
+
+function showPromo() {
+  document.getElementById("promo-success").style.display = "block";
+
+  document.getElementById("buttonId1").className = "selected-button";
+}
